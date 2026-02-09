efeb19a3abe5fdbf31c1f3d6c0afa8db64bd0a72
BoyCoder <chaxiu@users.noreply.github.com>
2026-02-09 16:37:19 +0800

    Apply local overrides at ba6080bbab070934ea6e870c5fc30dbf702eb445

--- comfy_api/latest/_input/__init__.py
+++ comfy_api/latest/_input/__init__.py
@@ -1,10 +1,15 @@
 from .basic_types import ImageInput, AudioInput, MaskInput, LatentInput
-from .video_types import VideoInput
+import importlib.util
+
+_VIDEO_AVAILABLE = importlib.util.find_spec("av") is not None
+if _VIDEO_AVAILABLE:
+    from .video_types import VideoInput
 
 __all__ = [
     "ImageInput",
     "AudioInput",
-    "VideoInput",
     "MaskInput",
     "LatentInput",
 ]
+if _VIDEO_AVAILABLE:
+    __all__.append("VideoInput")
